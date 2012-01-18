717b119ede99592f1f8febb7cce2b8ea350413aa
Ilshad Khabibullin <astoon@spacta.com>
2012-01-18 08:04:16 +0400

    personal dropdown menu

--- defpage/static/static/js/defpage.js
+++ defpage/static/static/js/defpage.js
@@ -1,1 +1,30 @@
 /* (c) defpage.com */
+
+$(function () {
+
+    $("#ajax_loader").ajaxStart(function () {
+	$(this).show();
+	$(".maybe_dim").css("opacity", 0.3);
+    }).ajaxStop(function () {
+	$(this).hide();
+	$(".maybe_dim").css("opacity", 1);
+    });
+
+    $("#back-top").hide();
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('#back-top').fadeIn();
+        } else {
+            $('#back-top').fadeOut();
+        }
+    });
+    $('#back-top a').click(function () {
+        $('body,html').animate({scrollTop: 0}, 800);
+        return false;
+    });
+
+    $("#usertitle").click(function (e) {
+	$(e.srcElement.parentNode).toggleClass('personal_menu_activated');
+    });
+
+});
